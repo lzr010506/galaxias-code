81c4c2e8dfc6c7c08455115e1ed42a93d6421abc
lzr010506 <l401113026@163.com>
2021-08-02 22:34:59 +0800

    1

---
+++ 2021 Nowcoder Multi-University Training 6/C.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define il inline
+#define db double
+#define ls rt << 1
+#define rs rt << 1 | 1
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define X first
+#define Y second
+#define pcc pair<char, char>
+#define vi vector<int>
+#define vl vector<ll>
+#define rep(i, x, y) for(int i = x; i <= y; i ++)
+#define rrep(i, x, y) for(int i = x; i >= y; i --)
+#define rep0(i, n) for(int i = 0; i < (n); i ++)
+#define per0(i, n) for(int i = (n) - 1; i >= 0; i --)
+#define ept 1e-9
+#define INF 0x3f3f3f3f
+#define sz(x) (x).size()
+#define ALL(x) (x).begin(), (x).end()
+using namespace std;
+static char ibuf[1 << 20]; char *fis = ibuf, *fit = ibuf;
+inline char readChar() { return fis == fit && (fit = (fis = ibuf) + fread(ibuf, 1, 1 << 20, stdin), fis == fit) ? EOF : *fis++; }
+inline int read() {
+    char c, _c; int x; for (c = readChar(); !isdigit(c); c = readChar());
+    for (x = 0; isdigit(c); c = readChar()) { x = x * 10 + c - '0'; } return x;
+}
+static char ostc[100], obuf[1 << 20]; int ol = 0; char *fos = obuf, *fot = obuf + (1 << 20) - 1;
+inline void writeChar(char c) { *fos++ = c; if (fos == fot) { fwrite(obuf, 1, fos - obuf, stdout); fos = obuf; } }
+inline void write(int n, char c) {
+    if (!n) { writeChar('0'); } else { while (n) { ostc[++ol] = n % 10 + 48, n /= 10; } }
+    for (; ol; ol--) { writeChar(ostc[ol]); } writeChar(c);
+}
+const int N = 310;
+const int Maxn = 310;
+struct Node
+{
+    int x, y, z;
+};
+vector<Node> ans;
+
+int main()
+{
+    int n = read();
+    rep(x, 1, n)
+        rep(y, x + 1, n)
+        {
+            int z = n - x - y;
+            if(z > y) ans.pb({x, y, z});
+            z = 2 * n - x - y;
+            if(z > y && z <= n) ans.pb({x, y, z});
+        }
+    printf("%d\n", (int)ans.size());
+    for(auto x : ans) printf("%d %d %d\n", x.x, x.y, x.z);
+}
